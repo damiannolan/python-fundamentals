293beb8e009089827d706a7e983c30119c158f0b
Damian Nolan <damiannolan@gmail.com>
2017-09-22 00:38:37 +0100

    Fixed guessing game, distinct attempts

--- guessingGame.py
+++ guessingGame.py
@@ -1,18 +1,26 @@
+# Probelm 5 - Guessing Game
+# Use random to get a random number between 1 and 20
+# Create a while loop and recieve input guesses
+# Append each try to a list of attempts - (useful later)
+# When the correct answer is entered - break the loop
+# Print the number of distinct tries using len() and set()
+
 import random
 
-tries = 0
+tries = []
 n = random.randint(1, 20)
 print("Guess a number between 1 and 20!")
 
 while 1: 
-    tries += 1
     guess = int(input())
 
     if guess < n:
-        print("Too low!")
+        print("Too low! Try again >:)")
+        tries.append(guess)
     elif guess > n:
-        print("Too high")
+        tries.append(guess)
+        print("Too high! Try again >:)")
     else:
-        print("Congrats, you guessed it in %d tries!" % tries)
-        break
-
+        tries.append(guess)
+        print("Congrats, you guessed it in %d tries!" % len(set(tries)))
+        break 
